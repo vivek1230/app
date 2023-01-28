8be4fa117c14450bfe938ce329b2212df4aa59c9
v0m03hj <vivek.maddheshiya@walmart.com>
2023-01-28 23:02:51 +0530

    Second commit

--- api/src/main/java/com/retailer/app/controllers/RetailerController.java
+++ api/src/main/java/com/retailer/app/controllers/RetailerController.java
@@ -2,30 +2,39 @@ package com.retailer.app.controllers;
 
 import com.retailer.app.dto.UserDetailsDto;
 import com.retailer.app.services.RetailerService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
 @RequestMapping("/v1")
+@Slf4j
+@RestController
+@Validated
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "App Api")
 public class RetailerController {
 
-    @Autowired
     RetailerService retailerService;
 
-    @GetMapping(path = "/userDetails", produces = "application/json")
-    public ResponseEntity<UserDetailsDto> getUserDetails(@RequestBody UserDetailsDto userDetailsDto) {
+    @PostMapping(path = "/userDetails", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserDetailsDto> addEmployee(@RequestBody UserDetailsDto userDetailsDto) {
 
-        UserDetailsDto UserDetailsDto = retailerService.getUserDetails(userDetailsDto);
+        UserDetailsDto UserDetailsDto = retailerService.addUserDetails(userDetailsDto);
         // return new ResponseEntity<>(UserDetailsDto, HttpStatus.OK);
         return ResponseEntity.ok(UserDetailsDto);
     }
 
-    @PostMapping(path = "/userDetails", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<UserDetailsDto> addEmployee(@PathVariable("email") String email) {
+    @GetMapping(path = "/userDetails/{email}", produces = "application/json")
+    public ResponseEntity<UserDetailsDto> getUserDetails(@PathVariable("email") String email) {
 
-        UserDetailsDto userDetailsDto = retailerService.addUserDetails(email);
+        UserDetailsDto userDetailsDto = retailerService.getUserDetails(email);
         // return new ResponseEntity<>(UserDetailsDto, HttpStatus.OK);
         return ResponseEntity.ok(userDetailsDto);
     }
